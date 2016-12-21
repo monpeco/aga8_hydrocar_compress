7cd2a429da8736304169078643aa4fe80654991b
monpeco <monpeco@gmail.com>
2016-12-21 17:56:43 +0000

    Add sql declare section and field-variable map

--- pred/inf_rec.c
+++ pred/inf_rec.c
@@ -8,6 +8,46 @@
 EXEC SQL INCLUDE parametros.h;
 
 /* ------------------------------------------------------------------------------ */
+/*                            DECLARACIÓN DE VARIABLES                            */
+/* ------------------------------------------------------------------------------ */
+char C010_par_fec_proceso[11];
+
+EXEC SQL BEGIN DECLARE SECTION;
+/* Declaracion estructuras recaudadores 
+| Query field      | Date type          | variables                |
+|------------------|--------------------|--------------------------|
+| COD_EMPRESA      | NUMBER (2)         | C002_cod_empresa[3]      | 
+| COD_OFICINA      | VARCHAR2 (4 Byte)  | C004_cod_oficina[5]      | 
+| COD_CAJERO       | VARCHAR2 (4 Byte)  | C004_cod_cajero[5]       | 
+| EST_PAGO         | VARCHAR2 (2 Byte)  | C002_est_pago[3]         | 
+| FEC_PAGO         | DATE               | C010_fec_pago[11]        | 
+| FEC_REGISTRO     | DATE               | C010_fec_registro[11]    | 
+| NRO_SUMINISTRO   | NUMBER (10)        | C010_nro_suministro[11]  | 
+| NRO_TRANSACCION  | NUMBER             | C022_nro_transaccion[23] | 
+| NRO_TRAN_RECA    | VARCHAR2 (50 Byte) | C050_nro_tran_reca[51]   | 
+| MTO_PAGO	       | NUMBER (15)        | C015_mto_pago[16]        | 
+| TIP_PAGO         | VARCHAR2 (3 Byte)  | C003_tip_pago[4]         | 
+
+*/
+
+
+char C002_cod_empresa[3]      ;    EXEC SQL VAR C002_cod_empresa IS STRING(3)      ;
+char C004_cod_oficina[5]      ;    EXEC SQL VAR C004_cod_oficina IS STRING(5)      ;
+char C004_cod_cajero[5]       ;    EXEC SQL VAR C004_cod_cajero IS STRING(5)       ;
+char C002_est_pago[3]         ;    EXEC SQL VAR C002_est_pago IS STRING(3)         ;
+char C010_fec_pago[11]        ;    EXEC SQL VAR C010_fec_pago IS STRING(11)        ;
+char C010_fec_registro[11]    ;    EXEC SQL VAR C010_fec_registro IS STRING(11)    ;
+char C010_nro_suministro[11]  ;    EXEC SQL VAR C010_nro_suministro IS STRING(11)  ;
+char C022_nro_transaccion[23] ;    EXEC SQL VAR C022_nro_transaccion IS STRING(23) ;
+char C050_nro_tran_reca[51]   ;    EXEC SQL VAR C050_nro_tran_reca IS STRING(51)   ;
+char C015_mto_pago[16]        ;    EXEC SQL VAR C015_mto_pago IS STRING(16)        ;
+char C003_tip_pago[4]         ;    EXEC SQL VAR C003_tip_pago IS STRING(4)         ;
+
+
+
+EXEC SQL END DECLARE SECTION;
+
+/* ------------------------------------------------------------------------------ */
 /*                            PROCESAMIENTO DE DATOS                              */
 /* ------------------------------------------------------------------------------ */
 
@@ -35,4 +75,4 @@ main(int argc,char **argv)
         printf("OK\n");
         exit(0);
     }
-}
+}               

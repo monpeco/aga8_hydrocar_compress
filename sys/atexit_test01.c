45f2e28dea860523ba50628cd6478b478f8095e4
monpeco <monpeco@gmail.com>
2016-12-26 20:14:12 +0000

    Add atexit example

---
+++ sys/atexit_test01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>      /* puts */
+#include <stdlib.h>     /* atexit */
+
+void fnExit1 (void){
+    puts ("Exit function 1.");
+}
+
+void fnExit2 (void){
+    puts ("Exit function 2.");
+}
+
+int main ()
+{
+    atexit (fnExit1);
+    atexit (fnExit2);
+    puts ("Main function.");
+    
+    /* Output:
+    Main function.
+    Exit function 2.
+    Exit function 1.
+    */
+    return 0;
+}

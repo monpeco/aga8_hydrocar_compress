3892711742b9ee9b58976fe8859537f3731c677e
monpeco <monpeco@gmail.com>
2017-01-12 13:09:35 +0000

    Testing 'date -d' to fecha_es

--- pred/test01.sh
+++ pred/test01.sh
@@ -1,7 +1,14 @@
-#/bin/bash
+#!/bin/bash
 
-fecha_ultimo_proceso="01/05/2017"
+# # Formato de fecha
+# formato_fecha () {
+#     fecha_ultimo_proceso=`cat fecha_inf_rec.txt`
+#     echo "fecha_ultimo_proceso: ${fecha_ultimo_proceso}"
 
-date -d "${fecha_ultimo_proceso} + 1 day" "+%d/%m/%y"
+# }
 
+fecha_es='20170101  +%Y%m%d '
+
+user_date=$( date -d $fecha_es )
     
+echo ${user_date '+%Y%m%d'}
